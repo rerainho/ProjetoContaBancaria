a727ddd9bb3db52c25c4ab3d11d07ce16a6fdc46
Renata Rainho <renata.rainho95@gmail.com>
2024-06-14 15:46:24 -0300

    subida de conta corrente e conta poupança

--- src/conta/Menu.java
+++ src/conta/Menu.java
@@ -1,33 +1,44 @@
 package conta;
 
 import java.util.Scanner;
-import conta.model.Conta;
+
 import conta.util.Cores;
+import conta.model.Conta;
+import conta.model.ContaCorrente;
+import conta.model.ContaPoupanca;
 
 public class Menu {
 
-	public static void main(String[] args) {
-		
-		// Teste da Classe Conta
-				Conta c1 = new Conta(1, 123, 1, "Adriana", 10000.0f);
-				c1.visualizar();
-				c1.sacar(12000.0f);
-				c1.visualizar();
-				c1.depositar(5000.0f);
-				c1.visualizar();
+	public static Scanner leia = new Scanner(System.in);
 
-		
-		// Aplicação de uma conta bancária	
-		Scanner leia = new Scanner(System.in);
+	public static void main(String[] args) {
 
 		int opcao;
+
+		// Teste da Classe Conta, foi deletado ao incluir o abstract
+		
+		// Teste da Classe Conta Corrente
+		ContaCorrente cc1 = new ContaCorrente(1, 123, 1, "José da Silva", 0.0f, 1000.0f);
+		cc1.visualizar();
+		cc1.sacar(12000.0f);
+		cc1.visualizar();
+		cc1.depositar(5000.0f);
+		cc1.visualizar();
+
+		// Teste da Classe Conta Poupança
+		ContaPoupanca cp1 = new ContaPoupanca(2, 123, 2, "Maria dos Santos", 100000.0f, 15);
+		cp1.visualizar();
+		cp1.sacar(1000.0f);
+		cp1.visualizar();
+		cp1.depositar(5000.0f);
+		cp1.visualizar();
 
 		while (true) {
 
 			System.out.println(Cores.TEXT_YELLOW + Cores.ANSI_BLACK_BACKGROUND
 					+ "*****************************************************");
 			System.out.println("                                                     ");
-			System.out.println("                BANCO DO BRAZIL COM Z                ");
+			System.out.println("                BANCO RAINHO                         ");
 			System.out.println("                                                     ");
 			System.out.println("*****************************************************");
 			System.out.println("                                                     ");
@@ -48,52 +59,52 @@ public class Menu {
 			opcao = leia.nextInt();
 
 			if (opcao == 9) {
-				System.out.println(Cores.TEXT_WHITE_BOLD + "\nBanco do Brazil com Z - O seu Futuro começa aqui!");
+				System.out.println(Cores.TEXT_WHITE_BOLD + "\nBanco Rainho - O seu Futuro começa aqui!");
 				sobre();
-                 leia.close();
+				leia.close();
 				System.exit(0);
 			}
 
 			switch (opcao) {
-				case 1:
-					System.out.println(Cores.TEXT_WHITE_BOLD + "Criar Conta\n\n");
+			case 1:
+				System.out.println(Cores.TEXT_WHITE_BOLD + "Criar Conta\n\n");
 
-					break;
-				case 2:
-					System.out.println(Cores.TEXT_WHITE_BOLD + "Listar todas as Contas\n\n");
+				break;
+			case 2:
+				System.out.println(Cores.TEXT_WHITE_BOLD + "Listar todas as Contas\n\n");
 
-					break;
-				case 3:
-					System.out.println(Cores.TEXT_WHITE_BOLD + "Consultar dados da Conta - por número\n\n");
+				break;
+			case 3:
+				System.out.println(Cores.TEXT_WHITE_BOLD + "Consultar dados da Conta - por número\n\n");
 
-					break;
-				case 4:
-					System.out.println(Cores.TEXT_WHITE_BOLD + "Atualizar dados da Conta\n\n");
+				break;
+			case 4:
+				System.out.println(Cores.TEXT_WHITE_BOLD + "Atualizar dados da Conta\n\n");
 
-					break;
-				case 5:
-					System.out.println(Cores.TEXT_WHITE_BOLD + "Apagar a Conta\n\n");
+				break;
+			case 5:
+				System.out.println(Cores.TEXT_WHITE_BOLD + "Apagar a Conta\n\n");
 
-					break;
-				case 6:
-					System.out.println(Cores.TEXT_WHITE_BOLD + "Saque\n\n");
+				break;
+			case 6:
+				System.out.println(Cores.TEXT_WHITE_BOLD + "Saque\n\n");
 
-					break;
-				case 7:
-					System.out.println(Cores.TEXT_WHITE_BOLD + "Depósito\n\n");
+				break;
+			case 7:
+				System.out.println(Cores.TEXT_WHITE_BOLD + "Depósito\n\n");
 
-					break;
-				case 8:
-					System.out.println(Cores.TEXT_WHITE_BOLD + "Transferência entre Contas\n\n");
+				break;
+			case 8:
+				System.out.println(Cores.TEXT_WHITE_BOLD + "Transferência entre Contas\n\n");
 
-					break;
-				default:
-					System.out.println(Cores.TEXT_RED_BOLD + "\nOpção Inválida!\n");
-					break;
+				break;
+			default:
+				System.out.println(Cores.TEXT_RED_BOLD + "\nOpção Inválida!\n");
+				break;
 			}
 		}
 	}
-    
+
 	public static void sobre() {
 		System.out.println("\n*********************************************************");
 		System.out.println("Projeto Desenvolvido por: ");
